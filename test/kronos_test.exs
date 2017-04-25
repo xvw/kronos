e40babb84cc4b54711f35f3fb6b789ed96e855d1
xvw <xavier.vdw@gmail.com>
2017-04-26 00:34:24 +0200

    Append tests

--- test/kronos_test.exs
+++ test/kronos_test.exs
@@ -11,16 +11,23 @@ defmodule KronosTest do
     assert Kronos.to_datetime!(t) == @dt
   end
 
+  test "Kronos.t creation failure" do 
+    r = Kronos.new({-2, 10, 10},{-3, 12, 68})
+    assert {:error, :invalid_date} == r
+  end
+
   test "Kronos.t arithmetics operations" do 
     day = @dt.day + 2
     min = @dt.minute + 12
 
     use Mizur.Infix, only: [+: 2]
     t = Kronos.new!(@ts) + Kronos.day(2) + Kronos.minute(12)
+    
     m =
       @dt 
       |> Map.put(:day, day)
       |> Map.put(:minute, min)
+
     assert Kronos.to_datetime!(t) == m
 
   end
