c5688b5c1056fdfe4a2790df7960253ed990e13f
xvw <xavier.vdw@gmail.com>
2017-04-25 12:40:31 +0200

    Initialize dependancies

--- mix.exs
+++ mix.exs
@@ -2,12 +2,17 @@ defmodule Kronos.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :kronos,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :kronos,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      name: "Mizur",
+      source_url: "https://github.com/xvw/kronos",
+      homepage_url: "https://github.com/xvw/kronos",
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
@@ -28,6 +33,10 @@ defmodule Kronos.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:mizur, "~> 0.1.0"},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
+    ]
   end
 end
